48c16704bb94650d193c449650fdcfbbc8225711
Fredrik Karlsson <fredrikkarlsson320@gmail.com>
2026-03-17 10:40:33 +0100

    change button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,19 @@ import AddStudents from "#/components/add-students";
 import Matrix from "#/components/matrix";
 import { Button } from "#/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
-import { UserRoundPlusIcon } from "lucide-react";
+import { UserCogIcon } from "lucide-react";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
-    <div className="h-screen flex flex-col px-4 py-8 bg-linear-to-r from-teal-400 to-yellow-200">
-      <header className="w-full flex justify-center items-center gap-2">
+    <div className="h-screen flex flex-col px-4 py-8 bg-linear-to-r from-yellow-300 via-yellow-200 to-yellow-300">
+      <header className="relative w-full flex justify-center items-center gap-2">
         <h1 className="text-teal-900 text-center">Ambitionsmatris</h1>
         <AddStudents>
-          <Button variant="aluna" size="icon-lg">
-            <UserRoundPlusIcon className="size-6" />
+          <Button variant="aluna" className="absolute right-0 top-0">
+            Redigera
+            <UserCogIcon className="size-6" />
           </Button>
         </AddStudents>
       </header>
