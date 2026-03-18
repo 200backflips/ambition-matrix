f8af8a9955c5fee636e1667dabc00c8577b0f57d
200backflips <200backflips@users.noreply.github.com>
2026-03-18 16:08:34 +0000

    deploy: eff6f8e3aefc118124a47f480537d390db2bcaa3

--- assets/sneek-C7uj917S.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-C6lAREBg.js";function n(){return o.jsx("div",{children:"dis is sneek route"})}export{n as component};
---
+++ assets/sneek-CeVXD4uH.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-C4VOZqvQ.js";function n(){return o.jsx("div",{children:"dis is sneek route"})}export{n as component};
